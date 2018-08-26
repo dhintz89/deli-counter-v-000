4ee0d8b842ba992eeceb9ff6fc77d76b97d9e7ca
Dan Hintz <dhintz@live.com>
2018-08-26 22:32:06 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,3 +14,13 @@ def take_a_number(katz_deli, name)
   katz_deli.push(name)
   puts "Welcome, #{name}. You are number #{katz_deli.index(name)+1} in line."
 end
+
+def now_serving(katz_deli)
+  if katz_deli.size == 0
+    puts "There is nobody waiting to be served!"
+  else
+    current_customer = katz_deli.shift()
+    puts "Currently serving #{current_customer}."
+  end
+end
+  
